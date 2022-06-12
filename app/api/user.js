56fb596d173d94f8d90848d58b2f29ad220f2ed4
Royce <royceang96@gmail.com>
2022-06-12 23:21:26 +0800

    setup basic

---
+++ app/api/user.js
@@ -0,0 +1,25 @@
+import client from "./client";
+
+/*
+* List all end points here
+*/
+const endpoint = "/User";
+const user_login = endpoint + "/Login";
+
+
+
+
+/*
+* List all functions here
+*/
+
+
+
+
+
+
+
+
+/*
+* Expose your end points here
+*/
