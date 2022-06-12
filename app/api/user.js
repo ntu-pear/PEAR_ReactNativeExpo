51be5196ddb2c4dc2271d3ad58534b21eb61281d
Royce <royceang96@gmail.com>
2022-06-12 23:32:17 +0800

    Included login

--- app/api/user.js
+++ app/api/user.js
@@ -4,16 +4,33 @@ import client from "./client";
 * List all end points here
 */
 const endpoint = "/User";
-const user_login = endpoint + "/Login";
-
-
-
+const userLogin = endpoint + "/Login";
+const userUpdate = endpoint + "/Update";
+const userDelete = endpoint + "/delete";
+const userRefreshToken = endpoint + "/RefereshToken";
+const userLogout = endpoint + "/Logout";
+const userResetPassword = endpoint + "/ResetPassword";
 
 /*
 * List all functions here
 */
 
+// GET REQUESTS
 
+
+// POST REQUESTS
+const loginUser = (email, role, password) => {
+    const data = new FormData();
+    data.append("Email", email);
+    data.append("Role", role);
+    data.append("Password", password);
+
+    console.log(data);
+    // Note: client.post accepts 3 parameters. (1) endpoint (2) data and (3) onUploadProgress -- this is optional
+    return client.post(userLogin, data)
+}
+
+// UPDATE REQUESTS
 
 
 
@@ -23,3 +40,6 @@ const user_login = endpoint + "/Login";
 /*
 * Expose your end points here
 */
+export default {
+    loginUser,
+}
