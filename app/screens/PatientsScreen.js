60e8b09736dc9c23f95c3f0e59cc0c9d62c87f53
Royce <royceang96@gmail.com>
2022-07-31 18:18:34 +0800

    some cahnges

--- app/screens/PatientsScreen.js
+++ app/screens/PatientsScreen.js
@@ -1,20 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { Center, Image, VStack, Box } from "native-base";
+import { Center, Image, VStack, Box, ScrollView } from "native-base";
 import patientApi from "../api/patient";
 import useCheckExpiredThenLogOut from "../hooks/useCheckExpiredThenLogOut";
 import PatientScreenCard from "../components/PatientScreenCard";
 import colors from "../config/colors";
+import PatientProfile from "../components/PatientProfile";
 
 function PatientsScreen() {
-  const [listOfPatients, setListOfPatients] = useState({});
+  const [listOfPatients, setListOfPatients] = useState();
   const checkExpiredLogOutHook = useCheckExpiredThenLogOut();
 
   useEffect(() => {
     // Reference https://stackoverflow.com/questions/21518381/proper-way-to-wait-for-one-function-to-finish-before-continuing
     // Resolved the issue of `setListOfPatients` before successfully calling getPatient api.
     const promiseFunction = async () => {
-      await getListOfPatients();
+      const response = await getListOfPatients();
+      setListOfPatients(response.data);
     };
     promiseFunction();
   }, []);
@@ -26,23 +28,22 @@ function PatientsScreen() {
       checkExpiredLogOutHook.handleLogOut(response);
       return;
     }
-    // Save the PatientData
-    setListOfPatients(response.data);
+    return response;
   };
 
   return (
     <Center>
-      <VStack>
-        <Box
-          bg={colors.light_var1}
-          minW="90%"
-          minH="20%"
-          overflow="hidden"
-          rounded="lg"
-        >
-          <PatientScreenCard />
-        </Box>
-      </VStack>
+      <ScrollView>
+        <VStack>
+          {listOfPatients ? (
+            listOfPatients.map((item, index) => (
+              <PatientScreenCard patientProfile={item} key={index} />
+            ))
+          ) : (
+            <Text> Loading... </Text>
+          )}
+        </VStack>
+      </ScrollView>
     </Center>
   );
 }
