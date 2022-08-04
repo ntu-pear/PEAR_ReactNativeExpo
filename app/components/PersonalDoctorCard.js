0852ac41652e6bd260173629b87693c2e3c825dc
Royce <royceang96@gmail.com>
2022-08-04 18:29:08 +0800

    included doctor note

--- app/components/PersonalDoctorCard.js
+++ app/components/PersonalDoctorCard.js
@@ -1,14 +1,50 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet } from "react-native";
+import { Stack, Text, FormControl, TextArea } from "native-base";
+import colors from "../config/colors";
 
 function PersonalDoctorCard() {
-    return (
-        <View>
-            <Text></Text>
-        </View>
-    )
+  return (
+    <Stack>
+      <Text
+        color={colors.black_var1}
+        fontFamily="Helvetica"
+        fontSize="2xl"
+        fontWeight="semibold"
+      >
+        Doctor's Notes
+      </Text>
+      <FormControl>
+        <Stack space={0} alignItems="flex-start" flexWrap={"wrap"}>
+          <FormControl.Label
+            width="100%"
+            _text={{
+              fontFamily: "Helvetica",
+              fontSize: "lg",
+              fontWeight: "thin",
+            }}
+          >
+            Medical Notes
+          </FormControl.Label>
+          <TextArea
+            color={colors.black_var1}
+            fontFamily="Helvetica"
+            fontSize="lg"
+            isReadOnly={true}
+            input="lg"
+            ml="-2.5"
+            minH="8%"
+            maxH="40%"
+            variant="unstyled"
+            value={"test"}
+            w="100%"
+          />
+        </Stack>
+      </FormControl>
+    </Stack>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
 
 export default PersonalDoctorCard;
--- app/screens/PatientInformationScreen.js
+++ app/screens/PatientInformationScreen.js
@@ -14,6 +14,7 @@ import {
 import colors from "../config/colors";
 import PersonalInformationCard from "../components/PersonalInformationCard";
 import PersonalPreferenceCard from "../components/PersonalPreferenceCard";
+import PersonalDoctorCard from "../components/PersonalDoctorCard";
 
 function PatientInformationScreen(props) {
   const { displayPicUrl, firstName, lastName } = props.route.params;
@@ -26,55 +27,57 @@ function PatientInformationScreen(props) {
   return (
     <Center minH="100%" backgroundColor={colors.white_var1}>
       <ScrollView>
-          <Box alignItems="Center">
-            <AspectRatio w="100%" ratio={16 / 9}>
-              <Image
-                source={{ uri: `${displayPicUrl}` }}
-                alt="patientInformationImage"
-              />
-            </AspectRatio>
-            <Center
-              position="absolute"
-              bg={colors.primary_overlay_color}
-              width="100%"
-              height="100%"
-            ></Center>
-            <Center position="absolute" px="5%" py="10%">
-              <VStack>
-                <Center
-                  alignItems="flext-start"
-                  _text={{
-                    color: `${colors.white_var1}`,
-                    fontFamily: "Helvetica",
-                    fontSize: "2xl",
-                    fontWeight: "500",
-                  }}
-                >
-                  You're caring for
-                </Center>
-                <Center
-                  alignItems="flext-start"
-                  _text={{
-                    color: `${colors.white_var1}`,
-                    fontFamily: "Helvetica",
-                    fontSize: "2xl",
-                    fontWeight: "500",
-                  }}
-                >
-                  {`${firstName} ${lastName}`}
-                </Center>
-              </VStack>
-            </Center>
-          </Box>
-          <VStack maxW="100%" mt="2.5">
-            <Stack ml="5" mr="5" space={5}>
-              <Divider mt="2"/>
-              <PersonalInformationCard patientInformation={props} />
-              <Divider />
-              <PersonalPreferenceCard patientInformation={props} />
-              <Divider />
-            </Stack>
-          </VStack>
+        <Box alignItems="Center">
+          <AspectRatio w="100%" ratio={16 / 9}>
+            <Image
+              source={{ uri: `${displayPicUrl}` }}
+              alt="patientInformationImage"
+            />
+          </AspectRatio>
+          <Center
+            position="absolute"
+            bg={colors.primary_overlay_color}
+            width="100%"
+            height="100%"
+          ></Center>
+          <Center position="absolute" px="5%" py="10%">
+            <VStack>
+              <Center
+                alignItems="flext-start"
+                _text={{
+                  color: `${colors.white_var1}`,
+                  fontFamily: "Helvetica",
+                  fontSize: "2xl",
+                  fontWeight: "500",
+                }}
+              >
+                You're caring for
+              </Center>
+              <Center
+                alignItems="flext-start"
+                _text={{
+                  color: `${colors.white_var1}`,
+                  fontFamily: "Helvetica",
+                  fontSize: "2xl",
+                  fontWeight: "500",
+                }}
+              >
+                {`${firstName} ${lastName}`}
+              </Center>
+            </VStack>
+          </Center>
+        </Box>
+        <VStack maxW="100%" mt="2.5">
+          <Stack ml="5" mr="5" space={5}>
+            <Divider mt="2" />
+            <PersonalInformationCard patientInformation={props} />
+            <Divider />
+            <PersonalPreferenceCard patientInformation={props} />
+            <Divider />
+            <PersonalDoctorCard />
+            <Divider />
+          </Stack>
+        </VStack>
       </ScrollView>
     </Center>
   );
