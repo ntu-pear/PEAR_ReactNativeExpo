07bf69e2d226c9cce0e4cb6bb02c793479742eca
Royce <royceang96@gmail.com>
2022-08-04 19:35:31 +0800

    initial look on social history card

--- app/components/PersonalSocialHistory.js
+++ app/components/PersonalSocialHistory.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet } from "react-native";
 import {
-  Avatar,
-  Center,
   Stack,
   Text,
   Input,
   FormControl,
   HStack,
-  TextArea,
+  Select,
+  CheckIcon,
 } from "native-base";
 import colors from "../config/colors";
 
 function PersonalSocialHistory() {
+  const [isEditMode, setIsEditMode] = useState(false);
+  let [service, setService] = useState("testhaha");
+
   return (
     <Stack alignItems="flex-start">
       <Text
@@ -22,6 +24,70 @@ function PersonalSocialHistory() {
         fontWeight="semibold"
       >
         Social History
+      </Text>
+      <Text
+        color={colors.primary_overlay_color}
+        fontFamily="Helvetica"
+        fontSize="md"
+        fontWeight="hairline"
+      >
+        About
+      </Text>
+      <FormControl maxW="50%">
+        <HStack space={2}>
+          <FormControl.Label
+            _text={{
+              fontFamily: "Helvetica",
+              fontSize: "lg",
+              fontWeight: "thin",
+            }}
+          >
+            Living with
+          </FormControl.Label>
+          {isEditMode ? (
+            <Select
+              fontFamily="Helvetica"
+              fontSize="lg"
+              label="hello"
+              minW="100%"
+              onValueChange={(itemValue) => setService(itemValue)}
+              placeholder={service}
+              selectedValue={service}
+              variant="underlined"
+              _selectedItem={{
+                endIcon: (
+                  <CheckIcon
+                    size={5}
+                    fontFamily="Helvetica"
+                    fontSize="lg"
+                    color={colors.pink}
+                  />
+                ),
+              }}
+            >
+              <Select.Item label="test1" value="test1" />
+            </Select>
+          ) : (
+            <Input
+              color={colors.black_var1}
+              fontFamily="Helvetica"
+              fontSize="lg"
+              isReadOnly={true}
+              variant="unstyled"
+              value={"test"}
+              w="100%"
+            />
+          )}
+        </HStack>
+      </FormControl>
+      <Text
+        color={colors.primary_overlay_color}
+        fontFamily="Helvetica"
+        fontSize="md"
+        fontWeight="hairline"
+        mt="5"
+      >
+        Lifestyle
       </Text>
     </Stack>
   );
