067ce877c6c0e3c5020d480459ab6ee0a2b2ea10
Royce <royceang96@gmail.com>
2022-08-05 22:06:46 +0800

    included error handling

--- app/screens/PatientInformationScreen.js
+++ app/screens/PatientInformationScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Platform } from "react-native";
+import { StyleSheet, Platform, View } from "react-native";
 import {
   Center,
   VStack,
@@ -10,6 +10,11 @@ import {
   AspectRatio,
   Image,
   useToast,
+  Alert,
+  CloseIcon,
+  HStack,
+  Text,
+  IconButton,
 } from "native-base";
 import colors from "../config/colors";
 import typography from "../config/typography";
@@ -24,6 +29,7 @@ import doctorNoteApi from "../api/doctorNote";
 import guardianApi from "../api/guardian";
 import socialHistoryApi from "../api/socialHistory";
 import useApi from "../hooks/useApi";
+import AppButton from "../components/AppButton";
 
 function PatientInformationScreen(props) {
   const { displayPicUrl, firstName, lastName, patientID } = props.route.params;
@@ -35,24 +41,67 @@ function PatientInformationScreen(props) {
   const getSocialHistory = useApi(socialHistoryApi.getSocialHistory);
 
   useEffect(() => {
-    getDoctorNote.request((patientID));
-    getPatientGuardian.request((patientID));
-    getSocialHistory.request((patientID));
+    getDoctorNote.request(patientID);
+    getPatientGuardian.request(patientID);
+    getSocialHistory.request(patientID);
   }, []);
 
-  const showToast = (errorMsg, position) => {
-    toast.show({
-      title: errorMsg,
-      placement: position,
-    });
+  /*
+   * Purpose: Handle API error if fetch fails
+   */
+  const handleError = () => {
+    if (getDoctorNote.error) getDoctorNote.request(patientID);
+    if (getPatientGuardian.error) getPatientGuardian.request(patientID);
+    if (getSocialHistory.error) getSocialHistory.request(patientID);
   };
 
   return (
     <>
-      {getDoctorNote.loading || getPatientGuardian.loading || getSocialHistory.loading ? (
+      {getDoctorNote.loading ||
+      getPatientGuardian.loading ||
+      getSocialHistory.loading ? (
         <ActivityIndicator visible={true} />
       ) : (
         <Center minH="100%" backgroundColor={colors.white_var1}>
+          {(getDoctorNote.error ||
+            getPatientGuardian.error ||
+            getSocialHistory.error) && (
+            <Box h="100%">
+              <Alert w="100%" status="error">
+                <VStack space={2} flexShrink={1} w="100%">
+                  <HStack
+                    flexShrink={1}
+                    space={2}
+                    justifyContent="space-between"
+                  >
+                    <HStack space={2} flexShrink={1}>
+                      <Alert.Icon mt="1" />
+                      <Text fontSize="md" color="coolGray.800">
+                        {`Unable to retrieve api data. Try again? Or Relogin`}
+                      </Text>
+                    </HStack>
+                    <IconButton
+                      variant="unstyled"
+                      _focus={{
+                        borderWidth: 0,
+                      }}
+                      icon={<CloseIcon size="3" />}
+                      _icon={{
+                        color: "coolGray.600",
+                      }}
+                    />
+                  </HStack>
+                </VStack>
+              </Alert>
+              <Box position="fixed" my="50%" w="60%" mx="auto">
+                <AppButton
+                  title="Try Again"
+                  color={"red"}
+                  onPress={handleError}
+                />
+              </Box>
+            </Box>
+          )}
           <ScrollView>
             <Box>
               <AspectRatio w="100%" ratio={16 / 9}>
@@ -105,11 +154,11 @@ function PatientInformationScreen(props) {
                 <Divider />
                 <PersonalDoctorCard doctorNote={getDoctorNote.data} />
                 <Divider />
-                <PersonalGuardianCard patientGuardian={getPatientGuardian.data} />
-                <Divider />
-                <PersonalSocialHistory
-                  socialHistory={getSocialHistory.data}
+                <PersonalGuardianCard
+                  patientGuardian={getPatientGuardian.data}
                 />
+                <Divider />
+                <PersonalSocialHistory socialHistory={getSocialHistory.data} />
               </Stack>
             </VStack>
           </ScrollView>
