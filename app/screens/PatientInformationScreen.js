e3f97e270395a3a397cb58cd518feef24b814111
Royce <royceang96@gmail.com>
2022-08-05 22:07:24 +0800

    removed redundant import

--- app/screens/PatientInformationScreen.js
+++ app/screens/PatientInformationScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Platform, View } from "react-native";
+import { StyleSheet, Platform } from "react-native";
 import {
   Center,
   VStack,
