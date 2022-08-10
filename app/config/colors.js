165852ee0812b1e2228eaf6e28eb371a6f933473
Royce <royceang96@gmail.com>
2022-08-10 19:31:06 +0800

    test

--- app/config/colors.js
+++ app/config/colors.js
@@ -6,30 +6,30 @@
  * Refer to this: https://www.color-hex.com/
  */
 export default {
-  primary: "#E8DCCC",
-  secondary: "#F3ECE4", // off-white
-  black: "#373234",
-  gray: "#ABABAB",
-  black_var2: "#3C4043",
-  red: "#FD4734",
-  light_gray: "#595959",
-  medium: "#6e6969", // place-holder text color
-  light: "#f8f4f4",
-  light_var1: "#E8DCCC",
-  lighter: "#f2eee9",
-  lighter_var2: "#f5f0e9",
+  primary: '#E8DCCC',
+  secondary: '#F3ECE4', // off-white
+  black: '#373234',
+  gray: '#ABABAB',
+  black_var2: '#3C4043',
+  red: '#FD4734',
+  light_gray: '#595959',
+  medium: '#6e6969', // place-holder text color
+  light: '#f8f4f4',
+  light_var1: '#E8DCCC',
+  lighter: '#f2eee9',
+  lighter_var2: '#f5f0e9',
   /*
    ***** Note: Bottom Colors are the
    ***** Primary Colors used in this app
    **** Inspired by Airbnb's design
    */
-  black_var1: "#484848", // ** Frequently used colors throughout app **
-  green: "#019192",
-  green_lighter: "#66bdbd",
-  pink: "#FF585D", // ** Frequently used colors throughout app **
-  pink_lighter: "#ff9396",
-  primary_gray: "#D4D4D4",
-  primary_overlay_color: "#00000080", // ligher black color
-  white: "#f3f6f4",
-  white_var1: "#FFFFFF",
+  black_var1: '#484848', // ** Frequently used colors throughout app **
+  green: '#019192',
+  green_lighter: '#66bdbd',
+  pink: '#FF585D', // ** Frequently used colors throughout app **
+  pink_lighter: '#ff9396',
+  primary_gray: '#D4D4D4',
+  primary_overlay_color: '#00000080', // ligher black color
+  white: '#f3f6f4',
+  white_var1: '#FFFFFF',
 };
