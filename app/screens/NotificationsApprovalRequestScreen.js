7a210a32b9842dd3af98f26895ab88a333f0a765
Royce <royceang96@gmail.com>
2022-08-13 18:11:59 +0800

    added notificationapprovalreqscreen

--- app/screens/NotificationsApprovalRequestScreen.js
+++ app/screens/NotificationsApprovalRequestScreen.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Text, Button, Box } from 'native-base';
+import { Text, Button, Box, VStack } from 'native-base';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import TopHeaderWithBackButton from '../components/TopHeaderWithBackButton';
 
 function NotificationsApprovalRequestScreen(props) {
   const { navigation } = props;
   const insets = useSafeAreaInsets();
 
   return (
-    <Box safeAreaTop="20">
-      <Text>This is NotificationsApprovalRequestScreen</Text>
-      <Button onPress={() => navigation.goBack()}> Go Back </Button>
+    <Box safeAreaTop>
+      <TopHeaderWithBackButton navigation={navigation} />
+      <Box w="90%" ml="5" mr="5" mt="5">
+        <Text>This is NotificationsApprovalRequestScreen</Text>
+      </Box>
     </Box>
   );
 }
