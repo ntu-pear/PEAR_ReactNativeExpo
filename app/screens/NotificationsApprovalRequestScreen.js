b7cc98c49567a0bde3083fab3ffebfab998a83e0
Royce <royceang96@gmail.com>
2022-08-13 20:05:11 +0800

    included button custom tabs

--- app/screens/NotificationsApprovalRequestScreen.js
+++ app/screens/NotificationsApprovalRequestScreen.js
@@ -1,19 +1,46 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Text, Button, Box, VStack } from 'native-base';
+import {
+  Text,
+  Button,
+  Box,
+  VStack,
+  Center,
+  HStack,
+  Divider,
+  ScrollView,
+} from 'native-base';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import TopHeaderWithBackButton from '../components/TopHeaderWithBackButton';
+import colors from '../config/colors';
 
 function NotificationsApprovalRequestScreen(props) {
   const { navigation } = props;
   const insets = useSafeAreaInsets();
 
   return (
-    <Box safeAreaTop>
+    <Box h="100%" w="100%" safeAreaTop>
       <TopHeaderWithBackButton navigation={navigation} />
       <Box w="90%" ml="5" mr="5" mt="5">
-        <Text>This is NotificationsApprovalRequestScreen</Text>
+        <VStack>
+          <ScrollView>
+            <Text>This is NotificationsApprovalRequestScreen</Text>
+          </ScrollView>
+        </VStack>
       </Box>
+
+      <Center
+        bg={colors.white_var1}
+        position="absolute"
+        bottom="0"
+        height="8%"
+        w="100%"
+        safeAreaBottom
+      >
+        <Divider mb="4" />
+        <HStack>
+          <Text> HHHA</Text>
+        </HStack>
+      </Center>
     </Box>
   );
 }
