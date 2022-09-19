27d8b4451a03148a59a8a9d46f8bf8c097843746
sohqianyi <60764194+sohqianyi@users.noreply.github.com>
2022-09-19 10:56:41 +0800

    :sparkles: [Patient] dynamically add guardian input

---
+++ app/components/AddPatientGuardian.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Box, Input, FormControl, Text, Select, Divider } from 'native-base';
+import colors from 'app/config/colors';
+import typography from 'app/config/typography';
+
+function AddPatientGuardian({ title }) {
+  return (
+    <Box>
+      {title == 1 ? null : <Divider mt={10} />}
+
+      <Text textAlign="center" marginTop={6} bold fontSize="2xl">
+        Guardian Information {title}
+      </Text>
+
+      <FormControl marginTop={4}>
+        <FormControl.Label>Guardian Name</FormControl.Label>
+        <Input placeholder="Guardian Name" />
+      </FormControl>
+
+      <FormControl>
+        <FormControl.Label>Guardian NRIC</FormControl.Label>
+        <Input placeholder="Guardian NRIC" />
+      </FormControl>
+
+      <FormControl>
+        <FormControl.Label>Guardian is Patient's</FormControl.Label>
+        <Select placeholder="Select">
+          <Select.Item label="Parent" value="parent" />
+          <Select.Item label="Child" value="child" />
+        </Select>
+      </FormControl>
+
+      <FormControl>
+        <FormControl.Label>Guardian's Handphone No.</FormControl.Label>
+        <Input placeholder="Guardian's Handphone Number" />
+      </FormControl>
+
+      <FormControl>
+        <FormControl.Label>Guardian's Home Telephone No.</FormControl.Label>
+        <Input placeholder="Guardian's Home Telephone Number" />
+      </FormControl>
+
+      <FormControl>
+        <FormControl.Label>Guardian Email </FormControl.Label>
+        <Input placeholder="Guardian Email" />
+      </FormControl>
+    </Box>
+  );
+}
+
+export default AddPatientGuardian;
