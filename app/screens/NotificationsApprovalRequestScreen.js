cfab4eb799e630f8f97d4e56dbd0b92bd6a40824
Royce <royceang96@gmail.com>
2022-09-23 09:51:15 +0800

    [Card-173] Include TextArea for including reasons

--- app/screens/NotificationsApprovalRequestScreen.js
+++ app/screens/NotificationsApprovalRequestScreen.js
@@ -1,6 +1,17 @@
-import React, { useContext, useState } from 'react';
-import { Platform } from 'react-native';
-import { Text, Box, VStack, ScrollView, Avatar, HStack } from 'native-base';
+import React, { useContext, useState, useRef } from 'react';
+import { Platform, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import {
+  Text,
+  Box,
+  VStack,
+  ScrollView,
+  Avatar,
+  HStack,
+  AlertDialog,
+  Button,
+  FormControl,
+  TextArea,
+} from 'native-base';
 import TopHeaderWithBackButton from 'app/components/TopHeaderWithBackButton';
 import BottomTabWithCustomizedButtons from 'app/components/BottomTabWithCustomizedButtons';
 import colors from 'app/config/colors';
@@ -13,6 +24,8 @@ function NotificationsApprovalRequestScreen(props) {
   const { senderName, senderPicUrl, message, notificationID } = route.params;
   const { setAcceptRejectNotifID } = useContext(AuthContext);
   const [isLoading, setIsLoading] = useState(false);
+  const [alertDialogIsOpen, setAlertDialogIsOpen] = useState(false);
+  const cancelRef = useRef(null);
 
   const leftBtnFn = () => {
     // setLoading
@@ -26,15 +39,28 @@ function NotificationsApprovalRequestScreen(props) {
     navigation.goBack();
   };
   const rightBtnFn = () => {
-    // setLoading
-    setIsLoading(true);
-    // (1) TODO: API Call to reject this LogID
-    // (2) Update parent screen
-    setAcceptRejectNotifID(notificationID);
-    // unSetLoading
-    setIsLoading(false);
-    // (3) Navigate back to parent screen
-    navigation.goBack();
+    setAlertDialogIsOpen(!alertDialogIsOpen);
+    // // setLoading
+    // setIsLoading(true);
+    // // (1) TODO: API Call to reject this LogID
+    // // (2) Update parent screen
+    // setAcceptRejectNotifID(notificationID);
+    // // unSetLoading
+    // setIsLoading(false);
+    // // (3) Navigate back to parent screen
+    // navigation.goBack();
+  };
+
+  const handleAddRejectComment = () => {
+    // (1) Set Loading
+    // (2) API - Notification Action API Call -- to add comments
+    // (3) API - Set Notification as Read
+    // (4) unSet Loading
+    // (5) Navigate back to parent screen
+  };
+
+  const onClose = () => {
+    setAlertDialogIsOpen(!alertDialogIsOpen);
   };
 
   return (
@@ -44,51 +70,93 @@ function NotificationsApprovalRequestScreen(props) {
       ) : (
         <Box h="100%" w="100%" safeAreaTop>
           <TopHeaderWithBackButton navigation={navigation} />
-          <Box w="90%" ml="5" mr="5" mt="5">
-            <VStack space={5}>
-              <HStack justifyContent="space-between" alignItems="center">
-                <Text
-                  color={colors.black_var1}
-                  fontFamily={
-                    Platform.OS === 'ios' ? 'Helvetica' : typography.android
-                  }
-                  fontWeight="bold"
-                  fontSize="md"
-                >
-                  {senderName} has requested for an update
-                </Text>
-                <Avatar
-                  size="md"
-                  bg={colors.pink}
-                  source={{
-                    uri: senderPicUrl
-                      ? senderPicUrl
-                      : 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-                  }}
-                >
-                  --
-                </Avatar>
-              </HStack>
-              <ScrollView h="100%" w="100%">
-                <Box>
+          <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <Box w="90%" ml="5" mr="5" mt="5">
+              <VStack space={5}>
+                <HStack justifyContent="space-between" alignItems="center">
                   <Text
                     color={colors.black_var1}
                     fontFamily={
                       Platform.OS === 'ios' ? 'Helvetica' : typography.android
                     }
-                    fontWeight="hairline"
+                    fontWeight="bold"
                     fontSize="md"
                   >
-                    {message}
+                    {senderName} has requested for an update
                   </Text>
-                </Box>
-              </ScrollView>
-            </VStack>
-          </Box>
+                  <Avatar
+                    size="md"
+                    bg={colors.pink}
+                    source={{
+                      uri: senderPicUrl
+                        ? senderPicUrl
+                        : 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+                    }}
+                  >
+                    --
+                  </Avatar>
+                </HStack>
+                <ScrollView h="100%" w="100%">
+                  <Box>
+                    <Text
+                      color={colors.black_var1}
+                      fontFamily={
+                        Platform.OS === 'ios' ? 'Helvetica' : typography.android
+                      }
+                      fontWeight="hairline"
+                      fontSize="md"
+                    >
+                      {message}
+                    </Text>
+                  </Box>
+                </ScrollView>
+              </VStack>
+            </Box>
+          </TouchableWithoutFeedback>
+
+          <AlertDialog
+            leastDestructiveRef={cancelRef}
+            isOpen={alertDialogIsOpen}
+            onClose={onClose}
+          >
+            <AlertDialog.Content>
+              <AlertDialog.CloseButton />
+              <AlertDialog.Header>Add Reason For Rejection</AlertDialog.Header>
+              <AlertDialog.Body>
+                <FormControl>
+                  <FormControl.Label
+                    _text={{
+                      fontSize: 'md',
+                      fontWeight: 'medium',
+                    }}
+                  >
+                    Reasons
+                  </FormControl.Label>
+                  <TextArea numberOfLines={6} />
+                </FormControl>
+              </AlertDialog.Body>
+              <AlertDialog.Footer>
+                <Button
+                  w="30%"
+                  size="md"
+                  bg={colors.pink}
+                  _text={{
+                    color: `${colors.white_var1}`,
+                    fontFamily:
+                      Platform.OS === 'ios' ? 'Helvetica' : typography.android,
+                    fontSize: 'sm',
+                  }}
+                >
+                  Confirm
+                </Button>
+              </AlertDialog.Footer>
+            </AlertDialog.Content>
+          </AlertDialog>
 
           <BottomTabWithCustomizedButtons
             leftButtonText="Accept"
             rightButtonText="Reject"
+            popOverWithForm={true}
             leftBtnFn={leftBtnFn}
             rightBtnFn={rightBtnFn}
           />
