312048da94b2c6653af85938be36718520954ead
kane <bwu009@e.ntu.edu.sg>
2022-10-06 15:48:10 +0800

    add branch protection hooks

--- app/screens/__test__/AccountScreen.test.js
+++ app/screens/__test__/AccountScreen.test.js
@@ -7,16 +7,7 @@ const customRender = (component) => {
     <AuthContext.Provider
       value={{
         user: {
-          aud: 'https://coremvc.fyp2017.com/',
-          email: 'jess@gmail.com',
-          exp: 1663242907,
-          iat: 1663235707,
-          iss: 'https://coremvc.fyp2017.com/',
-          jti: 'f49487a7-9aea-4abe-a4cd-d306a98ffb05',
-          nbf: 1663235697,
-          role: 'Supervisor',
           sub: 'Jessica Sim',
-          userID: 'B22698B8-42A2-4115-9631-1C2D1E2AC5F4',
         },
       }}
     >
