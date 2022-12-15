d6913d2f5729af4856cb1d93392006c14f4e8b4f
kane <bwu009@e.ntu.edu.sg>
2022-12-15 11:10:35 +0800

    Added test for WelcomeScreen

---
+++ app/screens/__test__/WelcomeScreen.test.js
@@ -0,0 +1,50 @@
+/**
+ * @jest-environment node
+ */
+import {
+  cleanup,
+  render,
+  screen,
+  fireEvent,
+} from '@testing-library/react-native';
+import { NativeBaseProvider } from 'native-base';
+import WelcomeScreen from 'app/screens/WelcomeScreen';
+import '@testing-library/jest-native/extend-expect';
+import errors from 'app/config/errors';
+
+jest.mock('../../hooks/useApiHandler');
+// we need to pass this in to NativeBaseProvider else the content within would
+// not be rendered
+const inset = {
+  frame: { x: 0, y: 0, width: 0, height: 0 },
+  insets: { top: 0, left: 0, right: 0, bottom: 0 },
+};
+afterEach(cleanup);
+describe('Test Login', () => {
+  test('Wrong credentials', async () => {
+    const welcomeScreen = render(
+      <NativeBaseProvider initialWindowMetrics={inset}>
+        <WelcomeScreen />
+      </NativeBaseProvider>,
+    );
+    const emailInput = welcomeScreen.getByPlaceholderText('jess@gmail.com');
+    const passwordInput = welcomeScreen.getByPlaceholderText('Password');
+    const loginButton = welcomeScreen.getByText('Login');
+    expect(emailInput).toBeVisible();
+    expect(passwordInput).toBeVisible();
+    expect(loginButton).toBeVisible();
+    fireEvent.changeText(emailInput, 'wronguser');
+    fireEvent.changeText(passwordInput, 'wrongpassword');
+    fireEvent.press(loginButton);
+    // without the timeout the test framework can't find the error on the screen
+    // since it's an asynchronous process to query the login api with the username
+    // and password and re-render the UI
+    expect(
+      await screen.findByText(
+        errors.loginError,
+        { exact: false },
+        { timeout: 4000 },
+      ),
+    ).not.toBeNull();
+  });
+});
