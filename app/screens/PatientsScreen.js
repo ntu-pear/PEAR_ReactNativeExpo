fafe16c72c446916a8fd4dd012161fee801a43d0
sohqianyi <60764194+sohqianyi@users.noreply.github.com>
2023-01-03 15:19:26 +0800

    [get patients] update code according to backend

--- app/screens/PatientsScreen.js
+++ app/screens/PatientsScreen.js
@@ -34,11 +34,11 @@ function PatientsScreen(props) {
     const response = await patientApi.getPatientList();
     if (!response.ok) {
       // Check if token has expired, if yes, proceed to log out
-      checkExpiredLogOutHook.handleLogOut(response);
+      checkExpiredLogOutHook.handleLogOut(response.data);
       return;
     }
     setIsLoading(false);
-    return response;
+    return response.data;
   };
 
   const handleFabOnPress = () => {
