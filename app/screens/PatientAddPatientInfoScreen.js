00ddf2fa42b3d8e7948c8ca9b991485548e8a63c
sohqianyi <60764194+sohqianyi@users.noreply.github.com>
2023-01-16 18:38:34 +0800

    :bug: (fix) datepicker issue for android + create datepicker component

--- app/screens/PatientAddPatientInfoScreen.js
+++ app/screens/PatientAddPatientInfoScreen.js
@@ -180,6 +180,7 @@ export function PatientAddPatientInfoScreen(props) {
               </HStack>
             </Radio.Group>
           </FormControl>
+
           {/* Reference: https://github.com/react-native-datetimepicker/datetimepicker
           TODO: Align to the left*/}
           <DatePickerComponent
