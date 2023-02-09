3bccdb40dd15729d4375e7766bd38fc590ca77d1
kane <bodong.is@gmail.com>
2023-02-09 16:17:05 +0800

    introduce mocked api call to test (#54)

--- app/screens/__test__/WelcomeScreen.test.js
+++ app/screens/__test__/WelcomeScreen.test.js
@@ -7,8 +7,15 @@ import WelcomeScreen from 'app/screens/WelcomeScreen';
 import '@testing-library/jest-native/extend-expect';
 import errors from 'app/config/errors';
 import { act } from 'react-test-renderer';
+import user from 'app/api/user';
 
 jest.mock('../../hooks/useApiHandler');
+jest.mock('../../api/user', () => ({
+  loginUser: jest.fn(async (_email, _role, _password) => ({
+    ok: false,
+  })),
+}));
+jest.setTimeout(10000);
 // we need to pass this in to NativeBaseProvider else the content within would
 // not be rendered
 const inset = {
@@ -28,14 +35,16 @@ describe('Test Login', () => {
     expect(emailInput).toBeVisible();
     expect(passwordInput).toBeVisible();
     expect(loginButton).toBeVisible();
-    act(() => {
-      fireEvent.changeText(emailInput, 'wronguser');
-      fireEvent.changeText(passwordInput, 'wrongpassword');
-      fireEvent.press(loginButton);
-    });
-    // without the timeout the test framework can't find the error on the screen
-    // since it's an asynchronous process to query the login api with the username
-    // and password and re-render the UI
+    fireEvent.changeText(emailInput, 'wronguser');
+    fireEvent.changeText(passwordInput, 'wrongpassword');
+    fireEvent.press(loginButton);
+    expect(user.loginUser).toBeCalledTimes(1);
+    expect(user.loginUser).toBeCalledWith(
+      'wronguser',
+      'Supervisor',
+      'wrongpassword',
+    );
+    // timeout is to buffer for re-rendering of component
     expect(
       await screen.findByText(
         errors.loginError,
