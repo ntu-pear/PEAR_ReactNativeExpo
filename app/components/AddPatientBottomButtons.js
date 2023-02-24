bdfbf4a373d0fb597a46b4bf31591a9bbbdf1c4d
sohqianyi <60764194+sohqianyi@users.noreply.github.com>
2023-02-24 15:28:10 +0800

    :fire: (wip) Joi to Yup validator

--- app/components/AddPatientBottomButtons.js
+++ app/components/AddPatientBottomButtons.js
@@ -17,44 +17,48 @@ function AddPatientBottomButtons({
   submit = false,
   formData = null,
   max = null,
+  validateStep = null,
 }) {
   const navigation = useNavigation();
 
   // useNavigate() hook cannot work on mobile
+  // eslint-disable-next-line
   const navigate = Platform.OS === 'web' ? useNavigate() : null;
 
   const onPressSubmit = async () => {
-    const result = await patientApi.addPatient(formData);
-    console.log('resulttsssss', result);
+    const promiseResult = await validateStep(formData);
+    console.log(promiseResult, formData);
 
-    let alertTxt = '';
-    if (result.ok) {
-      const allocations = result.data.data.patientAllocationDTO;
-      const caregiver = allocations.caregiverName;
-      const doctor = allocations.doctorName;
-      const gameTherapist = allocations.gameTherapistName;
+    if (promiseResult.success) {
+      console.log('success');
+      const result = await patientApi.addPatient(formData);
 
-      // Alert.alert(
-      //   'Successfully added Patient',
-      //   `Patient has been allocated to\nCaregiver: ${caregiver}\nDoctor: ${doctor}\nGame Therapist: ${gameTherapist}`,
-      // );
-      // navigation.navigate(routes.PATIENTS_SCREEN);
-      alertTxt = `Successfully added Patient\nPatient has been allocated to\nCaregiver: ${caregiver}\nDoctor: ${doctor}\nGame Therapist: ${gameTherapist}`;
+      let alertTxt = '';
+      let alertTitle = '';
+      let alertDetails = '';
+
+      if (result.ok) {
+        const allocations = result.data.data.patientAllocationDTO;
+        const caregiver = allocations.caregiverName;
+        const doctor = allocations.doctorName;
+        const gameTherapist = allocations.gameTherapistName;
+
+        alertTitle = 'Successfully added Patient';
+        alertDetails = `Patient has been allocated to\nCaregiver: ${caregiver}\nDoctor: ${doctor}\nGame Therapist: ${gameTherapist}`;
+        alertTxt = alertTitle + alertDetails;
+        Platform.OS === 'web'
+          ? navigate('/' + routes.PATIENTS)
+          : navigation.navigate(routes.PATIENTS_SCREEN);
+      } else {
+        const errors = result.data.message;
+        alertTitle = 'Error in Adding Patient';
+        alertDetails = `\n${errors}.\n\nPlease try again.`;
+        alertTxt = alertTitle + alertDetails;
+      }
       Platform.OS === 'web'
-        ? navigate('/' + routes.PATIENTS)
-        : navigation.navigate(routes.PATIENTS_SCREEN);
-    } else {
-      const errors = result.data.message;
-      // for (const error in errors) {
-      //   str += errors[error] + '.\n';
-      // }
-      // Alert.alert(
-      //   'Error in Adding Patient',
-      //   `\n${errors}.\n\nPlease try again.`,
-      // );
-      alertTxt = `Error in Adding Patient\n${errors}.\n\nPlease try again.`;
+        ? alert(alertTxt)
+        : Alert.alert(alertTitle, alertDetails);
     }
-    Platform.OS === 'web' ? alert(alertTxt) : Alert.alert(alertTxt);
   };
 
   return (
@@ -133,7 +137,7 @@ function AddPatientBottomButtons({
         {nextQuestionHandler ? (
           <Button
             bg={colors.green}
-            onPress={nextQuestionHandler}
+            onPress={() => nextQuestionHandler(formData)}
             leftIcon={
               <Icon as={<MaterialIcons name="chevron-right" />} color="white" />
             }
--- app/screens/PatientAddScreen.js
+++ app/screens/PatientAddScreen.js
@@ -1,19 +1,28 @@
 import React, { useState } from 'react';
-import { PatientAddPatientInfoScreen } from 'app/screens/PatientAddPatientInfoScreen';
-import { PatientAddGuardianScreen } from 'app/screens/PatientAddGuardianScreen';
-import { PatientAddAllergyScreen } from 'app/screens/PatientAddAllergyScreen';
-import { PatientAddMedicalHistoryScreen } from 'app/screens/PatientAddMedicalHistoryScreen';
-import * as ImagePicker from 'expo-image-picker';
+import { Platform } from 'react-native';
 
-export function PatientAddScreen(props) {
+import PatientAddPatientInfoScreen from 'app/screens/PatientAddPatientInfoScreen';
+import PatientAddGuardianScreen from 'app/screens/PatientAddGuardianScreen';
+import PatientAddAllergyScreen from 'app/screens/PatientAddAllergyScreen';
+import * as ImagePicker from 'expo-image-picker';
+// import Joi from 'joi';
+import * as Yup from 'yup';
+
+function PatientAddScreen(props) {
   // state for steps
   const [step, setStep] = useState(1);
+
+  // state for datepicker
+  const [show, setShow] = useState({
+    DOB: false,
+    StartDate: false,
+    EndDate: false,
+  });
 
   // state for components
   const [componentList, setComponentList] = useState({
     guardian: [{}],
     allergy: [{}],
-    medical: [{}],
   });
 
   const newDate = new Date();
@@ -170,14 +179,15 @@ export function PatientAddScreen(props) {
 
   const patientData = {
     patientInfo: {
-      FirstName: 'First',
-      LastName: 'Last',
-      PreferredName: 'PName',
+      FirstName: 'Patient',
+      LastName: '',
+      PreferredName: 'Patient',
       PreferredLanguageListID: 1,
       NRIC: 'S0948274A',
       Address: 'MyHome',
-      HomeNo: '61236123',
-      HandphoneNo: '61236123',
+      TempAddress: '',
+      HomeNo: '',
+      HandphoneNo: '',
       Gender: 'M',
       DOB: newDate,
       StartDate: newDate,
@@ -187,11 +197,14 @@ export function PatientAddScreen(props) {
       AutoGame: true,
       IsActive: true,
       IsRespiteCare: false,
-      TempAddress: 'MyHome',
       TerminationReason: '',
       InactiveReason: '',
       ProfilePicture: '',
-      UploadProfilePicture: '',
+      UploadProfilePicture: {
+        uri: '',
+        name: '',
+        type: '',
+      },
     },
 
     guardianInfo: [
@@ -208,18 +221,9 @@ export function PatientAddScreen(props) {
 
     allergyInfo: [
       {
-        allergyName: '', // allergyList[0].allergyName
-        allergyReaction: '',
-        allergyNotes: '',
-      },
-    ],
-
-    medicalInfo: [
-      {
-        medicalDetails: '',
-        medicalInfo: '',
-        medicalNotes: '',
-        medicalDate: newDate,
+        AllergyListID: 3,
+        AllergyReactionListID: 3,
+        AllergyRemarks: 'gg',
       },
     ],
   };
@@ -236,38 +240,150 @@ export function PatientAddScreen(props) {
     }
   };
 
+  // const validateStep = (formData) => {
+  //   const stepSchema = schema[Object.keys(schema)[step - 1]];
+  //   const toValidate = formData[Object.keys(formData)[step - 1]];
+
+  //   // Validate the form data against the schema
+  //   const { error } = stepSchema.validate(toValidate, { abortEarly: false });
+
+  //   if (error) {
+  //     // If there are validation errors, return an object indicating that the validation failed
+  //     const errors = error.details.map((detail) => detail.message);
+  //     return { success: false, errors };
+  //   } else {
+  //     return { success: true };
+  //   }
+  // };
+
+  const validateStep = async (formData) => {
+    const stepSchema = schema.fields[Object.keys(formData)[step - 1]];
+    const toValidate = formData[Object.keys(formData)[step - 1]];
+    console.log(stepSchema, step);
+    try {
+      // Validate the form data against the schema
+      await stepSchema.validate(toValidate, { abortEarly: false });
+
+      return { success: true };
+    } catch (error) {
+      if (error.inner) {
+        const errors = error.inner.map((detail) => detail.message);
+        return { success: false, errors };
+      } else {
+        return { success: false, errors: [error.message] };
+      }
+    }
+  };
+
+  // for each step of form, validate the data against schema
   // function for going to next step by increasing step state by 1
   // and to set component list
-  const nextQuestionHandler = (page = '', list = []) => {
-    componentHandler(page, list);
-    setStep(step + 1);
+  const nextQuestionHandler = async (formData, page = '', list = []) => {
+    const promiseResult = await validateStep(formData);
+    console.log(promiseResult);
+    // If the validation is successful, continue to the next question
+    if (promiseResult.success) {
+      componentHandler(page, list);
+      setStep((prevStep) => prevStep + 1);
+    }
   };
 
   // function for going to previous step by decreasing step state by 1
   // and to set component list
   const prevQuestionHandler = (page = '', list = []) => {
     componentHandler(page, list);
-    setStep(step - 1);
+    setStep((prevStep) => prevStep - 1);
   };
+
+  // Reference: https://docs.expo.dev/versions/latest/sdk/imagepicker/
+  const pickImage = (page, input) => async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    console.log('after picking picture', result);
+    const uriStr = result.uri;
+    const fileName = result.uri.split('/').pop();
+    const fileType = fileName.split('.').pop();
+
+    if (!result.cancelled) {
+      var img = formData[page];
+      img[input] = {
+        uri: uriStr,
+        name: fileName.split('.')[0],
+        type: `image/${fileType}`,
+      };
+
+      setFormData((prevState) => ({
+        ...prevState,
+        [page]: img,
+      }));
+    }
+  };
+  // handling form input data by taking onchange value and updating our previous form data state
+  // const handleFormData =
+  //   (page = '', input, index = null) =>
+  //   (e, date = null) => {
+  //     if (page === 'patientInfo') {
+  //       date
+  //         ? setFormData((prevState) => ({
+  //             ...prevState,
+  //             [input]: date,
+  //           }))
+  //         : setFormData((prevState) => ({
+  //             ...prevState,
+  //             [input]: e,
+  //           }));
+  //     } else {
+  //       const newData = formData[page].slice();
+  //       date ? (newData[index][input] = date) : (newData[index][input] = e); // eg. guardianInfo[0].guardianName = e
+
+  //       setFormData((prevState) => ({
+  //         ...prevState,
+  //         [page]: newData,
+  //       }));
+  //     }
+  //   };
 
   const handleFormData =
     (page = '', input, index = null) =>
     (e, date = null) => {
+      // set show as false when date is selected on datepicker
+      if (Platform.OS === 'android') {
+        setShow((prevState) => ({
+          ...prevState,
+          [input]: false,
+        }));
+      }
       if (page === 'patientInfo') {
         const newData = formData[page];
-        newData[input] = date
-          ? date
-          : e['$d'] //e['$d']-check if input from MUI date-picker
-          ? e['$d']
-          : parseInt(e) // check if integer (for dropdown)
-          ? parseInt(e) // change to integer
-          : e; // eg. guardianInfo[0].FirstName = e
+
+        // additional check to convert HomeNo and HandphoneNo to string
+        if (input === 'HomeNo' || input === 'HandphoneNo') {
+          newData[input] = date
+            ? date
+            : e.$d //e['$d']-check if input from MUI date-picker
+            ? e.$d
+            : e.toString(); // convert to string
+        } else {
+          newData[input] = date
+            ? date
+            : e.$d //e['$d']-check if input from MUI date-picker
+            ? e.$d
+            : parseInt(e) // check if integer (for dropdown)
+            ? parseInt(e) // change to integer
+            : e; // eg. guardianInfo[0].FirstName = e
+        }
 
         setFormData((prevState) => ({
           ...prevState,
           [page]: newData,
         }));
       } else {
+        // guardianInfo or allergyInfo
         const newData = formData[page].slice();
 
         // additional check to convert ContactNo  to string
@@ -292,18 +408,18 @@ export function PatientAddScreen(props) {
     case 1:
       return (
         <PatientAddPatientInfoScreen
-          key={1}
           nextQuestionHandler={nextQuestionHandler}
           handleFormData={handleFormData}
           formData={formData}
           componentList={componentList}
           pickImage={pickImage}
+          show={show}
+          setShow={setShow}
         />
       );
     case 2:
       return (
         <PatientAddGuardianScreen
-          key={2}
           nextQuestionHandler={nextQuestionHandler}
           prevQuestionHandler={prevQuestionHandler}
           handleFormData={handleFormData}
@@ -315,25 +431,13 @@ export function PatientAddScreen(props) {
     case 3:
       return (
         <PatientAddAllergyScreen
-          key={3}
           nextQuestionHandler={nextQuestionHandler}
           prevQuestionHandler={prevQuestionHandler}
           handleFormData={handleFormData}
           formData={formData}
           setFormData={setFormData}
           componentList={componentList}
-        />
-      );
-    case 4:
-      return (
-        <PatientAddMedicalHistoryScreen
-          key={4}
-          nextQuestionHandler={nextQuestionHandler}
-          prevQuestionHandler={prevQuestionHandler}
-          handleFormData={handleFormData}
-          formData={formData}
-          setFormData={setFormData}
-          componentList={componentList}
+          validateStep={validateStep}
         />
       );
     default:
