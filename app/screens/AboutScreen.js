b569b86c7ba405ddbcb61c8e32b40c18237b0b3e
ruoqing11 <ruoqing11@users.noreply.github.com>
2023-03-14 17:35:54 +0800

    add about screen

--- app/screens/AboutScreen.js
+++ app/screens/AboutScreen.js
@@ -1,12 +1,47 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, StyleSheet } from 'react-native';
+import colors from 'app/config/colors';
+import { Center } from 'native-base';
 
 function AboutScreen() {
   return (
     <View>
-      <Text>This is about screen</Text>
+      <Center>
+        <View style={styles.logoContainer}>
+          <Image
+            source={require('../assets/pear_v2.png')}
+            style={styles.logo}
+          />
+          <Text style={styles.text}>
+            You have downloaded the latest version of PEAR.
+            {'\n\n\n'}
+            Version: 1.0.0
+            {'\n\n'}
+            Build Date: 18 August 2018
+          </Text>
+        </View>
+      </Center>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 100,
+    height: 150,
+    tintColor: colors.black,
+  },
+  logoContainer: {
+    top: 100,
+    alignItems: 'center',
+  },
+  text: {
+    paddingTop: 100,
+    fontSize: 20,
+    maxWidth: 300,
+    textAlign: 'center',
+    whiteSpace: 'pre-line',
+  },
+});
 
 export default AboutScreen;
