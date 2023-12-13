f1e034c63c222c4f01e51c6811435e9500044e7a
heri-ek <89896942+heri-ek@users.noreply.github.com>
2023-12-13 11:54:09 +0800

    added debug stuff

---
+++ app/navigation/DebugNavigator.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+// Import Constants from routes
+import routes from 'app/navigation/routes';
+import PatientAddAllergyScreen from 'app/screens/PatientAddAllergyScreen';
+import PatientAddPatientInfoScreen from 'app/screens/PatientAddPatientInfoScreen';
+import PatientAddScreen from 'app/screens/PatientAddScreen';
+import PatientDailyHighlights from 'app/components/PatientDailyHighlights';
+import AccountEditScreen from 'app/screens/AccountEditScreen';
+
+// Refer to this: https://reactnavigation.org/docs/hello-react-navigation
+const Stack = createNativeStackNavigator();
+
+// Refer to this for configuration: https://reactnavigation.org/docs/native-stack-navigator#options
+function DebugNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="xyz"
+        component={PatientAddScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default DebugNavigator;
