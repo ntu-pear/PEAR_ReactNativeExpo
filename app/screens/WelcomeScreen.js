8ce8a61d1966ade71c8cba853ff4f5aece108275
heri-ek <89896942+heri-ek@users.noreply.github.com>
2023-12-13 10:38:11 +0800

    changed to use refactored input fields

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useCallback } from 'react';
 import {
   ImageBackground,
   View,
@@ -11,7 +11,7 @@ import {
 } from 'react-native';
 
 // Custom Import from https://reactnativeelements.com/docs/
-import { Select, Input, Center, Icon, Box } from 'native-base';
+import { Center, Icon, Box } from 'native-base';
 import { MaterialIcons } from '@expo/vector-icons';
 import jwt_decode from 'jwt-decode';
 import AuthContext from 'app/auth/context';
@@ -25,36 +25,60 @@ import useApiHandler from 'app/hooks/useApiHandler';
 import routes from 'app/navigation/routes';
 
 // Import from components
-import AppText from 'app/components/AppText';
 import AppButton from 'app/components/AppButton';
 import ErrorMessage from 'app/components/ErrorMessage';
 
 // Import Api
 import userApi from 'app/api/user';
 import LoadingWheel from 'app/components/LoadingWheel';
+import InputField from 'app/components/input-fields/InputField';
+import SelectionInputField from 'app/components/input-fields/SelectionInputField';
+
 
 function WelcomeScreen(props) {
   const authContext = useContext(AuthContext);
-  const [role, setRole] = useState('Supervisor');
   const [show, setShow] = useState(false);
-  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [userRole, setUserRole] = useState('Supervisor');
   const [password, setPassword] = useState('');
-  const [loginFailed, setLoginFailed] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [isUsernameError, setIsUsernameError] = useState(false);
+  const [isPasswordError, setIsPasswordError] = useState(false);
+  const [isLoginError, setIsLoginError] = useState(false);
+
   const apiHandlerHook = useApiHandler();
+
+   /*
+   * All Api to be place here
+   */
+
+  /*
+   * Component Did Mount or useEffect() to be placed here
+   */
+
+  /*
+   * Deconstructor
+   * Note: Navigation is passed down as a prop from NativeStackNavigator
+   */
   const { navigation } = props;
 
   const onPressLogin = async () => {
     console.log('Starting login process...\n');
+    
     setIsLoading(true);
-    const result = await userApi.loginUser(email, role, password);
-    // if returned array is empty or error
+    setIsLoginError(false);    
+    
+    const result = await userApi.loginUser(username, userRole, password);
+
+    // if authentication unsucessful (returned array is empty or error)
     if (!result.ok) {
       console.log('Authentication error');
       setIsLoading(false);
-      setLoginFailed(true);
+      setIsLoginError(true);
       return;
     }
+
+    // if authentication successful
     console.log('Authenticated...');
     console.log('Storing token...');
     const user = jwt_decode(result.data.data.accessToken);
@@ -63,165 +87,156 @@ function WelcomeScreen(props) {
       'userRefreshToken',
       result.data.data.refreshToken,
     );
+
     // set api header if empty
     console.log('Tokens stored and setting header...');
     apiHandlerHook.setHeader();
     console.log('Header updated...');
     setIsLoading(false);
-    setLoginFailed(false);
+    setIsLoginError(false);
     console.log('Logging in!');
     authContext.setUser(user);
   };
 
-  const handleEmail = (e) => {
-    setEmail(e);
+  const handleUsernameChanged = (e) => {
+    setUsername(e);
   };
+  
+  const handleUserRoleChanged = (e) => {
+    setUserRole(userRoles[e-1])
+  }
 
-  const handlePassword = (e) => {
+  const handlePasswordChanged = (e) => {
     setPassword(e);
   };
+
+  const handleUsernameError = (e) => {
+    setIsUsernameError(e)
+  }
+
+  const handlePasswordError = (e) => {
+    setIsPasswordError(e)
+  }
+    
+  const userRoles = ['Supervisor', 'Guardian', 'Doctor', 'Caregiver', 'Nurse']
 
   return (
     <ImageBackground
       style={styles.background}
-      blurRadius={8}
+      blurRadius={9}
       source={require('../assets/login_background.jpg')}
     >
+
       <TouchableWithoutFeedback
         onPress={() => {
-          // Prevent keyboard dismiss for desktop web only
           if (Platform.OS !== 'web' || navigator.userAgent.includes('Mobile')) {
             Keyboard.dismiss();
           }
         }}
       >
-        <View testID={'loginContentContainer'}>
-          <View style={styles.logoContainer}>
-            <Image
-              source={require('../assets/pear_v2.png')}
-              style={styles.logo}
-            />
-            <AppText style={styles.tagLine}>PEAR</AppText>
-          </View>
-
-          <Center flex={1}>
+        <View testID={'loginContentContainer'} style={styles.overlay}>
+          <Center flex={1} style={styles.formContainer}>
             <View style={styles.credentialsContainer}>
-              <Input
-                accessible={true}
-                testID="username"
-                autoCapitalize="none"
-                bg={colors.gray}
-                borderRadius="25"
-                color={colors.black}
-                _focus={{
-                  bg: `${colors.lighter}`,
-                  borderColor: `${colors.secondary}`,
-                }}
-                fontFamily={
-                  Platform.OS === 'ios' ? typography.ios : typography.android
-                }
-                height="50"
-                InputLeftElement={
-                  <Icon
-                    as={<MaterialIcons name="person" />}
-                    size={5}
-                    ml="5"
-                    color={colors.black}
-                  />
-                }
-                onChangeText={handleEmail}
-                placeholder="Username/Email"
-                placeholderTextColor={colors.medium}
-                marginBottom="5"
-                size="18"
-                value={email}
-              />
-              <Select
-                accessibilityLabel="Select Role"
-                bg={colors.gray}
-                borderRadius="25"
-                color={colors.black}
-                fontFamily={
-                  Platform.OS === 'ios' ? typography.ios : typography.android
-                }
-                height="50"
-                minWidth="full"
-                minHeight="3%"
-                placeholder="Supervisor"
-                placeholderTextColor={colors.black}
-                onValueChange={(itemValue) => setRole(itemValue)}
-                selectedValue={role}
-                size="18"
-              >
-                <Select.Item label="Supervisor" value="Supervisor" />
-                <Select.Item label="Guardian" value="Guardian" />
-                <Select.Item label="Doctor" value="Doctor" />
-                <Select.Item label="Caregiver" value="Caregiver" />
-                <Select.Item label="Nurse" value="Nurse" />
-              </Select>
-              <Input
-                accessible={true}
-                testID="password"
-                autoCapitalize="none"
-                bg={colors.gray}
-                borderRadius="25"
-                color={colors.black}
-                fontFamily={
-                  Platform.OS === 'ios' ? typography.ios : typography.android
-                }
-                _focus={{
-                  bg: `${colors.lighter}`,
-                  borderColor: `${colors.secondary}`,
-                }}
-                height="50"
-                InputRightElement={
-                  <Icon
-                    as={
-                      <MaterialIcons
-                        name={show ? 'visibility' : 'visibility-off'}
-                      />
-                    }
-                    color={colors.black}
-                    mr="5"
-                    onPress={() => setShow(!show)}
-                    size={5}
-                  />
-                }
-                onChangeText={handlePassword}
-                placeholder="Password"
-                placeholderTextColor={colors.medium}
-                marginTop="5"
-                size="18"
-                value={password}
-                type={show ? 'text' : 'password'}
-              />
-            </View>
-            <Box>
-              <ErrorMessage
-                visible={loginFailed}
-                message={errors.loginError}
-                testID={'loginError'}
-              />
-            </Box>
-            <View style={styles.buttonsContainer}>
-              {isLoading ? (
-                <LoadingWheel />
-              ) : (
-                <AppButton
-                  title="Login"
-                  color="green"
-                  onPress={onPressLogin}
-                  testingID="Login"
+              <View style={styles.logoContainer}>
+                <Image
+                  source={require('../assets/pear_v2.png')}
+                  style={styles.logo}
                 />
-              )}
-            </View>
-            <View>
-              <Text
-                style={styles.underline}
-                onPress={() => navigation.navigate(routes.RESET_PASSWORD)}
-              >
-                Forgot Password?
-              </Text>
+                <Text style={styles.tagLine}>PEAR</Text>
+              </View>
+              <View style={[styles.inputContainer, styles.bottomMargin]}>
+                <InputField
+                  testID="username"
+                  isRequired
+                  showTitle={false}
+                  title="Username/Email"
+                  value={username}
+                  onChangeText={handleUsernameChanged}
+                  onChildData={handleUsernameError}
+                  InputLeftElement={
+                    <Icon
+                      as={<MaterialIcons name="person" />}
+                      size={5}
+                      ml="5"
+                    />
+                  }
+                />
+              </View>
+              <View style={[styles.inputContainer, styles.bottomMargin]}>
+                <SelectionInputField
+                  isRequired
+                  showTitle={false}
+                  title="Select Role"
+                  inputLeftElement={
+                    <Icon
+                      as={<MaterialIcons name="settings" />}
+                      size={5}
+                      ml="5"
+                    />
+                  }
+                  placeholder="Supervisor"
+                  onDataChange={handleUserRoleChanged}
+                  dataArray={userRoles}
+                />
+              </View>
+              <View style={styles.inputContainer}>
+                <InputField
+                  testID="password"
+                  isRequired
+                  showTitle={false}
+                  title="Password"
+                  value={password}
+                  type={show ? 'general' : 'password'}
+                  onChangeText={handlePasswordChanged}
+                  onChildData={handlePasswordError}
+                  InputLeftElement={
+                    <Icon
+                      as={<MaterialIcons name="lock" />}
+                      size={5}
+                      ml="5"
+                    />
+                  }
+                  InputRightElement={
+                    <Icon
+                      as={
+                        <MaterialIcons
+                          name={show ? 'visibility' : 'visibility-off'}
+                        />
+                      }
+                      color={colors.black}
+                      mr="5"
+                      onPress={() => setShow(!show)}
+                      size={5}
+                    />
+                  }
+                  />
+                </View>
+              <View style={styles.buttonsContainer}>
+                {isLoading ? (
+                  <LoadingWheel />
+                  ) : (
+                    <AppButton
+                    title="Login"
+                    color="green"
+                    onPress={onPressLogin}
+                    testingID="Login"
+                    />
+                    )}
+              </View>
+              <Box>
+                <ErrorMessage
+                  message={isLoginError ? errors.loginError: ''}
+                  testID={'loginError'}
+                  />
+              </Box>
+              <View>
+                <Text
+                  style={styles.underline}
+                  onPress={() => navigation.navigate(routes.RESET_PASSWORD)}
+                  >
+                  Forgot Password?
+                </Text>
+              </View>
             </View>
           </Center>
         </View>
@@ -232,27 +247,25 @@ function WelcomeScreen(props) {
 
 const styles = StyleSheet.create({
   background: {
-    flex: 1,
     alignItems: 'center',
+  },
+  overlay: {
+    backgroundColor: colors.secondary_overlay_color
+  },
+  formContainer: {
+    paddingHorizontal: 75
   },
   buttonsContainer: {
     width: '100%',
-    padding: 20,
-  },
-  buttonsContainerWeb: {
-    top: 130,
-    width: '100%',
-    padding: 20,
+    paddingVertical: 8
   },
   credentialsContainer: {
-    width: '90%',
-  },
-  credentialsContainerWeb: {
-    top: 130,
-    width: '90%',
-  },
-  errorsContainerWeb: {
-    top: 130,
+    backgroundColor: colors.white,
+    width: '100%',
+    paddingHorizontal: 50,
+    paddingTop: 25,
+    paddingBottom: 90,
+    borderRadius: 25  
   },
   logo: {
     width: 100,
@@ -260,20 +273,27 @@ const styles = StyleSheet.create({
     tintColor: colors.black,
   },
   logoContainer: {
-    top: 100,
+    padding: 50,
     alignItems: 'center',
   },
   tagLine: {
     fontWeight: 'bold',
-    paddingVertical: 800,
-    fontSize: 80,
+    marginTop: 14,
+    fontSize: 40,
+    color: colors.black,
   },
   underline: {
     textDecorationLine: 'underline',
+    alignSelf: 'center'
   },
-  underlineWeb: {
-    top: 130,
+  inputContainer: {
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'flex-start',
   },
+  bottomMargin: {
+    marginBottom: '0.5%'
+  }
 });
 
 export default WelcomeScreen;
