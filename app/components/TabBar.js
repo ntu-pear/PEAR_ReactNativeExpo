c4d03fd762c6da3ffe8a6b22016e75bcba855e23
heri-ek <89896942+heri-ek@users.noreply.github.com>
2024-02-21 13:06:06 +0800

    add tab bar component

---
+++ app/components/TabBar.js
@@ -0,0 +1,65 @@
+// Libs
+import { Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+// Configurations
+import colors from 'app/config/colors';
+import typography from 'app/config/typography';
+
+function TabBar({
+  TABS={},
+  curTab='',
+  setCurTab=()=>{},  
+  handleSwitchTab=()=>{}
+}) {  
+  // Switch between tabs
+  const handleOnToggleViewMode = (mode) => {
+    setCurTab(mode);    
+    handleSwitchTab(mode);
+  }
+
+  return (
+    <View style={styles.optionsContainer}>
+      {Object.keys(TABS).map(mode => (
+        <TouchableOpacity 
+          key={mode}
+          style={[styles.tab, ...curTab==TABS[mode] ? [styles.selectedTab] : []]}
+          onPress={() => handleOnToggleViewMode(TABS[mode])}
+          activeOpacity={1}
+          >
+            <Text style={[styles.tabText, ...curTab==TABS[mode] ? [styles.selectedTabText] : []]}>{mode}</Text>
+        </TouchableOpacity>
+
+      ))}            
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  optionsContainer: {
+    flexDirection: 'row',
+    paddingVertical: '1%',
+    paddingHorizontal: '2%',
+    alignSelf: 'flex-start',
+    flexWrap: 'wrap'
+  },
+  tab: {
+    padding: '1.5%',
+    flex: 0.5,
+  },
+  tabText: {
+    fontSize: 20,
+    textAlign: 'center',
+    fontFamily: Platform.OS === 'ios' ? typography.ios : typography.android,
+  },
+  selectedTab: {
+    borderBottomColor: colors.green,
+    borderBottomWidth: 3,
+  },
+  selectedTabText: {
+    fontWeight: 'bold',
+    color: colors.green,
+  }
+});
+
+export default TabBar;
