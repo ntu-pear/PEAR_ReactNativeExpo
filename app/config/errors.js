6d5cef80989c1a22d9c199be9fbacfd2d7150384
heri-ek <89896942+heri-ek@users.noreply.github.com>
2024-03-22 16:33:55 +0800

    add error code function

--- app/config/errors.js
+++ app/config/errors.js
@@ -11,3 +11,24 @@ export default {
   emailError: 'Invalid Email',
   duplicatePrefNameError: 'Preferred name is already in use. Please enter a different value.'
 };
+
+export const getErrorMsgFromCode = (codr) => {
+  switch(code) {
+    case 400: 
+      return "There was a problem with your request. Please double-check your inputs and try again.";
+    case 401: 
+      return "You are not authorized to access this resource. Please make sure you're logged in with the correct credentials.";
+    case 403: 
+      return "You don't have permission to access this resource. If you believe this is an error, please contact support for assistance.";
+    case 404: 
+      return "The resource you are looking for could not be found.";
+    case 500: 
+      return "Something went wrong on our end. Please try again later.";
+    case 503: 
+      return "The service is temporarily unavailable. Please try again later. If the problem persists, please contact support.";
+    default:
+      return "Something went wrong. Please try again later."
+
+  }
+
+}
