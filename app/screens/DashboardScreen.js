11bb1f1c01495942e96c4be05d39fd59ccfefcbe
Joel Tan <78136208+Joel-yj@users.noreply.github.com>
2024-03-22 14:54:46 +0800

    Revert "change scrollview to flatlist"
    This reverts commit 724a5d579b0214f8fa61ed845692ca96a2bf80d5.

--- app/screens/DashboardScreen.js
+++ app/screens/DashboardScreen.js
@@ -661,16 +661,16 @@ function DashboardScreen({ navigation }) {
                       </>
                     ) : null}
                   </Container>
-                  <FlatList
+                  <ScrollView
                     horizontal={true}
                     width="100%"
                     showsHorizontalScrollIndicator={false}
-                    data={item.activities}
                     contentContainerStyle={{flexGrow: 1, alignItems: 'center', justifyContent: item.activities.length > 0 ? 'flex-start': 'center'}}
-                    renderItem={({item:activity, index}) => {
-                      return (
+                  >
+                    <HStack>
+                      {item.activities.map((activity, i) => (
                         <ActivityCard
-                          key={index}
+                          key={i}
                           activityTitle={activity.activityTitle}
                           activityStartTime={activity.startTime}
                           activityEndTime={activity.endTime}
@@ -680,10 +680,10 @@ function DashboardScreen({ navigation }) {
                           patientID={item.patientID}
                           date={item.date}
                           navigation={navigation}
-                          />
-                      )
-                    }}
-                  />
+                        />
+                      ))}
+                    </HStack>                
+                  </ScrollView>
                 </HStack>
               </Box>
             )
