9cb7d5ca827f88f3d98efc9f3053964bae02d4da
Joel Tan <78136208+Joel-yj@users.noreply.github.com>
2024-03-22 14:57:17 +0800

    Revert "Merge branch 'siti-branch' into 23S1-APK-Generation"
    This reverts commit 02d5ddc077552f52f9b683c0567808f390ca8f8b, reversing
    changes made to 724a5d579b0214f8fa61ed845692ca96a2bf80d5.

--- app/screens/PatientProfileScreen.js
+++ app/screens/PatientProfileScreen.js
@@ -1,4 +1,3 @@
-// Libs
 import React, { useState, useEffect } from 'react';
 import { Dimensions } from 'react-native';
 import { Center, VStack, HStack, ScrollView, View } from 'native-base';
@@ -8,42 +7,26 @@ import {
   FontAwesome5,
   Ionicons,
 } from '@expo/vector-icons';
-
-// API
-import patientApi from 'app/api/patient';
-import doctorNoteApi from 'app/api/doctorNote';
-import guardianApi from 'app/api/guardian';
-import socialHistoryApi from 'app/api/socialHistory';
-
-// Config
-import colors from 'app/config/colors';
-import routes from 'app/navigation/routes';
-
-// Components
 import PatientInformationCard from 'app/components/PatientInformationCard';
 import PatientProfileCard from 'app/components/PatientProfileCard';
-import PatientInformationScreen from 'app/components/PatientInformationScreen';
+import colors from 'app/config/colors';
+import routes from 'app/navigation/routes';
+import patientApi from 'app/api/patient';
 import ActivityIndicator from 'app/components/ActivityIndicator';
 
 function PatientProfileScreen(props) {
   const { navigation, route } = props;
   const [patientProfile, setPatientProfile] = useState({});
-  const [doctorsNoteData, setDoctorNoteData] = useState([]);
-  const [guardianData, setGuardianData] = useState([]);
-  const [socialHistoryData, setSocialHistoryData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [patientID, setPatientID] = useState(route.params.id);
 
   useEffect(() => {
     // navigated from Highlights Modal or Dashboard Screen
+    // Fetch patient's data from patient api
     console.log('id', route.params.id);
     getPatient(route.params.id);
-    retrieveDoctorsNote(route.params.id);
-    retrieveGuardian(route.params.id);
-    retrieveSocialHistory(route.params.id);
   }, [route.params.id, route.params.patientProfile]);
 
-  // Retrieval of Patient Info, Doctor's Notes, Guardian Info and Social History
   const getPatient = async (id) => {
     setIsLoading(true);
     // setIsError(false);
@@ -61,37 +44,6 @@ function PatientProfileScreen(props) {
     // console.log('Request successful with response: ', response);
   };
 
-  const retrieveDoctorsNote = async (id) => {
-    const response = await doctorNoteApi.getDoctorNote(id);
-    if (!response.ok) {
-      console.log('Request failed with status code: ', response.status);
-      return;
-    }
-    setDoctorNoteData(response.data.data);
-  };
-
-  const retrieveGuardian = async (id) => {
-    const response = await guardianApi.getPatientGuardian(id, false);
-    if (!response.ok) {
-      console.log('Request failed with status code: ', response.status);
-      return;
-    }
-    setGuardianData(response.data.data);
-  };
-
-  const retrieveSocialHistory = async (id) => {
-    const response = await socialHistoryApi.getSocialHistory(id);
-    if (!response.ok) {
-      console.log('Request failed with status code: ', response.status);
-      return;
-    }
-    if (response.data.data === null) {
-      setSocialHistoryData([]);
-    } else {
-      setSocialHistoryData(response.data.data);
-    }
-  };
-
   const SCREEN_HEIGHT = Dimensions.get('window').height;
 
   return (
@@ -105,6 +57,7 @@ function PatientProfileScreen(props) {
             h="100%"
             contentContainerStyle={{
               flexGrow: 1,
+              display: 'flex',
               flexDirection: 'column',
             }}
           >
@@ -129,7 +82,7 @@ function PatientProfileScreen(props) {
                 mr="3%"
                 mt="6%"
                 h="100%"
-                style={{ flex: 1, height:500 }}
+                style={{ flex: 2 }}
               >
                 <HStack space={2} w="100%" h="30%">
                   <View w="30%" h="100%">
@@ -277,16 +230,6 @@ function PatientProfileScreen(props) {
                   </View>
                 </HStack>
               </VStack>
-              <View w="100%" style={{flex: 1}}>
-                <PatientInformationScreen 
-                  patientID={patientID}
-                  patientProfile={patientProfile}
-                  guardianData={guardianData}
-                  doctorsNoteData={doctorsNoteData}
-                  socialHistoryData={socialHistoryData}
-                />
-              </View>
-              
             </VStack>
           </ScrollView>
         </Center>
