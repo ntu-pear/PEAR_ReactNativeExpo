e0a7680f0cd8c7508022c87872b50410f6bf3a18
Nicole Veronica <nicoleveronica.su@gmail.com>
2025-02-18 01:12:28 +0800

    apk-18-2-2025

--- app/screens/AboutScreen.js
+++ app/screens/AboutScreen.js
@@ -30,7 +30,7 @@ function AboutScreen(props) {
             {'\n\n\n'}
             Version: 1.0.0
             {'\n\n'}
-            Build Date: 26 Jan 2025
+            Build Date: 18 Feb 2025
           </Text>
         </View>
       </Center>
